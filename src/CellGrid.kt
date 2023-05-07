e75712083676d55e87f651483468010197f17fbf
Paul Sobolik <psobolik@zoho.com>
2023-05-07 11:30:52 -0400

    Fixed faulty merge of previous changes.

--- src/CellGrid.kt
+++ src/CellGrid.kt
@@ -1,29 +1,30 @@
 package mazeLib
 
+import java.lang.Exception
+
 class CellGrid(val rows: Int, val cols: Int) : Iterator<Cell> {
-    private var next = Coordinates(-1, -1)
+    // Iterate array left to right, bottom to top
+    private var next = Coordinates(rows, -1)
 
     private val grid = Array(rows) { _ -> Array<Cell?>(cols) { _ -> null } }
 
     override fun hasNext(): Boolean {
-        return inBounds(Coordinates(0, next.col + 1))
-                || inBounds(Coordinates(next.row + 1, 0))
+        return areCoordinatesInBounds(Coordinates(0, next.col + 1))
+                || areCoordinatesInBounds(Coordinates(next.row - 1, 0))
     }
 
     override fun next(): Cell {
-        var result: Cell? = null
         var test = Coordinates(next.row, next.col + 1)
-        if (inBounds(test)) {
+        if (areCoordinatesInBounds(test)) {
             next = test
-            result = get(next)
-        } else {
-            test = Coordinates(next.row + 1, 0)
-            if (inBounds(test)) {
-                next = test
-                result = get(next)
-            }
+            return get(next)!!
         }
-        return result!!
+        test = Coordinates(next.row - 1, 0)
+        if (areCoordinatesInBounds(test)) {
+            next = test
+            return get(next)!!
+        }
+        throw Exception("Invalid call to CellGrid.next()")
     }
 
     operator fun get(coordinates: Coordinates): Cell? {
@@ -34,15 +35,7 @@ class CellGrid(val rows: Int, val cols: Int) : Iterator<Cell> {
         grid[coordinates.row][coordinates.col] = value
     }
 
-    fun inBounds(coordinates: Coordinates): Boolean {
-        return rowInBounds(coordinates.row) && colInBounds(coordinates.col)
-    }
-
-    private fun rowInBounds(row: Int): Boolean {
-        return row in 0 until rows
-    }
-
-    private fun colInBounds(col: Int): Boolean {
-        return col in 0 until cols
+    fun areCoordinatesInBounds(coordinates: Coordinates): Boolean {
+        return coordinates.row in 0 until rows && coordinates.col in 0 until cols
     }
 }
